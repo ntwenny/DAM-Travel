8b4f61570b96550fef58b841a2c4a174170d49b2
hawkings232 <asiankidjoe6@gmail.com>
2025-10-14 19:10:03 -0500

    feat: add tabBar icons for Scan and Diagnostics screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,24 @@
 import { Tabs } from "expo-router";
+import { ClipboardPlusIcon, FileScanIcon } from "lucide-react-native";
 
 export default function TabsLayout() {
     return (
         <Tabs screenOptions={{ headerShown: true }}>
-            <Tabs.Screen name="index" options={{ title: "Scan" }} />
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Scan",
+                    tabBarIcon: () => (
+                        <ClipboardPlusIcon size={24} color="white" />
+                    ),
+                }}
+            />
             <Tabs.Screen
                 name="diagnostics"
-                options={{ title: "Diagnostics" }}
+                options={{
+                    title: "Diagnostics",
+                    tabBarIcon: () => <FileScanIcon size={24} color="white" />,
+                }}
             />
         </Tabs>
     );
