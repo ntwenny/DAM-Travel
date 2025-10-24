d5e72b0ae9e97943385e0c078e69d7f47d9c7be1
hawkings232 <asiankidjoe6@gmail.com>
2025-10-24 16:38:48 -0500

    feat: implement tab navigation layout and add Scan and Diagnostics screens

---
+++ frontend/app/_layout.tsx
@@ -0,0 +1,9 @@
+import "@/global.css";
+import { Stack } from "expo-router";
+export default function RootLayout() {
+    return (
+        <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+    );
+}
