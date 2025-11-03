ca0cbc4c726e8851ce92ad7567ecf1bd446f97e7
hawkings232 <asiankidjoe6@gmail.com>
2025-11-03 13:51:53 -0600

    Add SignInForm and SignUpForm components with social connection options

---
+++ frontend/components/sign-in-form.tsx
@@ -0,0 +1,104 @@
+import { SocialConnections } from "@/components/social-connections";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import { Text } from "@/components/ui/text";
+import * as React from "react";
+import { Pressable, type TextInput, View } from "react-native";
+
+export function SignInForm() {
+    const passwordInputRef = React.useRef<TextInput>(null);
+
+    function onEmailSubmitEditing() {
+        passwordInputRef.current?.focus();
+    }
+
+    function onSubmit() {
+        // TODO: Submit form and navigate to protected screen if successful
+    }
+
+    return (
+        <View className="gap-6">
+            <Card className="border-border/0 sm:border-border shadow-none sm:shadow-sm sm:shadow-black/5">
+                <CardHeader>
+                    <CardTitle className="text-center text-xl sm:text-left">
+                        Sign in to your app
+                    </CardTitle>
+                    <CardDescription className="text-center sm:text-left">
+                        Welcome back! Please sign in to continue
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="gap-6">
+                    <View className="gap-6">
+                        <View className="gap-1.5">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                placeholder="m@example.com"
+                                keyboardType="email-address"
+                                autoComplete="email"
+                                autoCapitalize="none"
+                                onSubmitEditing={onEmailSubmitEditing}
+                                returnKeyType="next"
+                                submitBehavior="submit"
+                            />
+                        </View>
+                        <View className="gap-1.5">
+                            <View className="flex-row items-center">
+                                <Label htmlFor="password">Password</Label>
+                                <Button
+                                    variant="link"
+                                    size="sm"
+                                    className="web:h-fit ml-auto px-1 py-0 sm:h-4"
+                                    onPress={() => {
+                                        // TODO: Navigate to forgot password screen
+                                    }}
+                                >
+                                    Forgot your password?
+                                </Button>
+                            </View>
+                            <Input
+                                ref={passwordInputRef}
+                                id="password"
+                                secureTextEntry
+                                returnKeyType="send"
+                                onSubmitEditing={onSubmit}
+                            />
+                        </View>
+                        <Button className="w-full" onPress={onSubmit}>
+                            <Text>Continue</Text>
+                        </Button>
+                    </View>
+
+                    <View className="flex-row items-center">
+                        <Label>Don't have an account? </Label>
+                        <Button
+                            variant="link"
+                            size="sm"
+                            className="text-lg"
+                            onPress={() => {}}
+                        >
+                            Sign up
+                        </Button>
+                    </View>
+                    <View className="flex-row items-center">
+                        <Separator className="flex-1" />
+                        <Text className="text-muted-foreground px-4 text-sm">
+                            or
+                        </Text>
+                        <Separator className="flex-1" />
+                    </View>
+                    <SocialConnections />
+                </CardContent>
+            </Card>
+        </View>
+    );
+}
